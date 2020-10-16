eb2df44008d34183f6ec92e90b162fa9de8aa1c9
김시아 <kim4-3@naver.com>
2020-10-16 16:11:59 +0900

    db

--- siaApp/models.py
+++ siaApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ProjectList(models.Model):
+    proj_date = models.CharField(max_length = 30)
+    proj_group = models.CharField(max_length = 30)
+    proj_topic = model.TextField()
+    proj_overview = model.TextField()
+    proj_summary = model.TextField()
